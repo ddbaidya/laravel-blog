4dd926b9b39efef8dd7b46d9a46a14d22fa7a2bd
Debdulal Baidya <ovibaidya72@gmail.com>
2023-06-08 10:54:37 +0600

    Return user to admin or user login page based on route prefix

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,6 +12,10 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('login');
+        if ($request->expectsJson()) {
+            return null;
+        } else {
+            return ($request->route()->getPrefix() == 'admin') ? route('admin.login') : route('login');
+        }
     }
 }
